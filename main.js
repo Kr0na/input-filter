942cde6c9b3b96e36264d338de7f1a00adff653c
Alex Grand <alex@granted.name>
2015-11-17 06:03:06 +0200

    fixes with module system

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-export {
-    Input: require('./src/Input.js'),
-    InputFilter: require('./src/InputFilter.js'),
+export {default as Input} from './src/Input.js'
+export {default as InputFilter} from './src/InputFilter.js'
 
-    Bool: require('./src/filter/Bool.js'),
-    Float: require('./src/filter/Float.js'),
-    Integer: require('./src/filter/Integer.js'),
-    StringTrim: require('./src/filter/StringTrim.js'),
+export {default as Bool} from './src/filter/Bool.js'
+export {default as Float} from './src/filter/Float.js'
+export {default as Integer} from './src/filter/Integer.js'
+export {default as StringTrim} from './src/filter/StringTrim.js'
 
-    Between: require('./src/validator/Between.js'),
-    Callback: require('./src/validator/Callback.js'),
-    Date: require('./src/validator/Date.js'),
-    InArray: require('./src/validator/InArray.js'),
-    StringLength: require('./src/validator/StringLength.js'),
+export {default as Between} from './src/validator/Between.js'
+export {default as Callback} from './src/validator/Callback.js'
+export {default as Date} from './src/validator/Date.js'
+export {default as InArray} from './src/validator/InArray.js'
+export {default as StringLength} from './src/validator/StringLength.js'
 
-    FilterChain: require('./src/FilterChain.js'),
-    ValidatorChain: require('./src/ValidatorChain.js'),
-    PromiseHelper: require('./src/PromiseHelper.js')
-}
+export {default as FilterChain} from './src/FilterChain.js'
+export {default as ValidatorChain} from './src/ValidatorChain.js'
+export {default as PromiseHelper} from './src/PromiseHelper.js'
