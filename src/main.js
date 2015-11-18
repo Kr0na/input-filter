175cb6cd64becf163f699cd7d561782e1d2a7423
Alex Grand <alex@granted.name>
2015-11-18 18:48:23 +0200

    added build for node

---
+++ src/main.js
@@ -0,0 +1,17 @@
+export {default as Input} from './Input.js'
+export {default as InputFilter} from './InputFilter.js'
+
+export {default as Bool} from './filter/Bool.js'
+export {default as Float} from './filter/Float.js'
+export {default as Integer} from './filter/Integer.js'
+export {default as StringTrim} from './filter/StringTrim.js'
+
+export {default as Between} from './validator/Between.js'
+export {default as Callback} from './validator/Callback.js'
+export {default as Date} from './validator/Date.js'
+export {default as InArray} from './validator/InArray.js'
+export {default as StringLength} from './validator/StringLength.js'
+
+export {default as FilterChain} from './FilterChain.js'
+export {default as ValidatorChain} from './ValidatorChain.js'
+export {default as PromiseHelper} from './PromiseHelper.js'
